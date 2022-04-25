060892aacad2f7a0b6ff9499bf73c3b030412bf0
igruiz91 <igruiz91@gmail.com>
2022-04-25 23:03:32 +0200

    css battle

---
+++ Tests/jsisweird/test1.js
@@ -0,0 +1,20 @@
+console.log([1, 2, 4] + [4, 5, 6]);
+console.log(0.2 + 0.1 === 0.3);
+console.log(!!!true)
+console.log(true == "true");
+console.log("" - - "");
+console.log(null + 0);
+console.log(0/0);
+console.log(1/0>0 ** 1000);
+// console.log(true++);
+console.log(""-1);
+console.log((null-0) + "0");
+console.log(true + ("true"-0));
+console.log(!5 + !5);
+console.log([]+[]);
+console.log(1+2+"3");
+console.log(typeof NaN);
+console.log(undefined+false);
+console.log("" && -0);
+console.log(+!!NaN * "" - - [,]);
+console.log(1/0 > Math.pow(10,1000))
