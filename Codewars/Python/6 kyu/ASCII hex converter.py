4d4d89c2af0083aedb55a77803f3a7173e04b0ef
igruiz91 <igruiz91@gmail.com>
2022-12-15 23:25:53 -0500

    commit

--- Codewars/Python/6 kyu/ASCII hex converter.py
+++ Codewars/Python/6 kyu/ASCII hex converter.py
@@ -2,7 +2,9 @@ class Converter():
     @staticmethod
     def to_ascii(h):
       return "".join([hex(ord(x))[2:] for x in h])
-
+    
+    def to_ascii_up(h):
+      return h.decode("hex")
 
     @staticmethod
     def to_hex(s):
@@ -11,12 +13,16 @@ class Converter():
         resp+= "0x"+s[i:i+2]+" "
       return ''.join(chr(int(h, 16)) for h in resp.split())
 
+    def to_hex_up(s):
+      return s.encode("hex")
 
 s = "Look mom, no hands"
 
 
 a = Converter.to_ascii(s)
+a = Converter.to_ascii_up(s)
 b = Converter.to_hex(a)
+a = Converter.to_hex_up(a)
 
 print(a)
 print(b)
