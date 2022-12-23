95b0d6b0b76151f83760e2c6ccdee10dfadddb7c
igruiz91 <igruiz91@gmail.com>
2022-12-23 07:07:54 -0500

    yesterday simple train

--- Codewars/Javascript/7 kyu/II/Capitals first!.js
+++ Codewars/Javascript/7 kyu/II/Capitals first!.js
@@ -1,6 +1,14 @@
 function capitalsFirst(str) {
-  return str.split(' ').sort((a,b) => b[1].localeCompare(a[1])).join(' ');
+  let cap = [], low = []
+  for(let word of str.split(' ')){
+    let firstChar = word[0]
+    if(!/[a-z]/i.test(firstChar)) continue
+    if (firstChar.toUpperCase() === word[0]) cap.push(word);
+    else low.push(word)
+  }
+  return cap.concat(low).join(' ');
 }
 
 
+console.log(capitalsFirst("123 You Me baby and"));
 console.log(capitalsFirst("hey You, Sort me Already!"));
