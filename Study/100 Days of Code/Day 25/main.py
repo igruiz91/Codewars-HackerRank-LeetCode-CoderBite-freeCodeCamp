d3eb1d42ca4f82167b97d36a2be45ae6cd0810d5
igruiz91 <igruiz91@gmail.com>
2023-12-12 23:44:11 -0500

    day 25 done

--- Study/100 Days of Code/Day 25/main.py
+++ Study/100 Days of Code/Day 25/main.py
@@ -1,22 +1,48 @@
-import pandas
+import turtle
 import os
+import pandas
 
-dir = os.path.dirname(os.path.abspath(__file__))
-route = os.path.join(dir,"Squirrel_Data.csv")
-
-data = pandas.read_csv(route)
-
-
-cinnamons = data[data["Primary Fur Color"] == "Cinnamon"]
-blacks = data[data["Primary Fur Color"] == "Black"]
-grays = data[data["Primary Fur Color"] == "Gray"]
+route = os.path.dirname(__file__)
+data = os.path.join(route, '50_states.csv')
+states_data = pandas.read_csv(data)
+image = os.path.join(route, "blank_states_img.gif")
 
 
-colors_dict = {
-  "Primary Fur Color" : ["Black", "Gray", "Cinnamon"],
-  "Count" : [len(blacks),  len(grays),len(cinnamons)],
-}
+corrects = []
+
+def print_states(info):
+  state = turtle.Turtle()
+  state.penup()
+  state.hideturtle()
+  state.goto(int(info.x.item()), int(info.y.item()))
+  state.write(f"{info.state.item()}")
+
+def states_to_learn():
+  missing_states = []
+  for state in states_data.state:
+    if state not in corrects:
+      missing_states.append(state)
+  new_data = pandas.DataFrame(missing_states)
+  new_data.to_csv(os.path.join(route, "learn_states.csv"))
 
 
-colors_data = pandas.DataFrame(colors_dict)
-colors_data.to_csv(os.path.join(dir,"colors_data.csv"))
+screen  = turtle.Screen()
+screen.title("U.S States Game")
+screen.addshape(image)
+
+turtle.shape(image)
+
+while len(corrects)<50:
+  answer_state = screen.textinput(title = f"Guess the State {len(corrects)}/50", prompt= "What's another state's name?")
+  if answer_state.lower() == "exit":
+    states_to_learn()
+    break
+  for state in states_data.state:
+    if state.lower() == answer_state:
+      print_states(states_data[states_data.state == state])
+      corrects.append(answer_state.capitalize())
+
+
+
+turtle.mainloop()
+
